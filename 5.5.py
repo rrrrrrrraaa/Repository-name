ebd3515f185fef2a7e2c680401b69649a178d110
rrrrrrrraaa <72706825+rrrrrrrraaa@users.noreply.github.com>
2020-10-12 03:00:30 +0300

    Create 5.5.py

---
+++ 5.5.py
@@ -0,0 +1,15 @@
+def price(k,l):
+    if k==343:
+        r=15
+    elif k==381:
+            r==13
+    elif k==485:
+                r==11
+    return l*r
+print("код города")
+k=int(input())
+print("длительность")
+l=int(input())
+print("Стоимость ",price(k,l))
+print("type 'end' to exit ")
+e=str(input())
